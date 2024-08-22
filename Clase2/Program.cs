511f49971a3fcb3bb95144d46d1327d45dda2515
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2024-08-22 09:08:04 -0300

    Add project files.

---
+++ Clase2/Program.cs
@@ -0,0 +1,170 @@
+﻿//short corto = -32000;
+//ushort cortoPositivo = 64000;
+
+//int entero = 2147483647;
+//uint natural = 4000000000;
+
+//long largo = 9223372036854775807;
+//ulong largoPositivo = 9923372036854775807;
+
+//Console.WriteLine($"El tamanio del tipo de dato short es {sizeof(short)}");
+//Console.WriteLine($"El tamanio del tipo de dato ushort es {sizeof(ushort)}");
+//Console.WriteLine($"El tamanio del tipo de dato int es {sizeof(int)}");
+//Console.WriteLine($"El tamanio del tipo de dato uint es {sizeof(uint)}");
+//Console.WriteLine($"El tamanio del tipo de dato long es {sizeof(long)}");
+//Console.WriteLine($"El tamanio del tipo de dato ulong es {sizeof(ulong)}");
+
+
+//float unDecimal = 123.34532f;
+//double unDoble = 12345.12345;
+
+//Console.WriteLine($"El tamanio del tipo de float long es {sizeof(float)}");
+//Console.WriteLine($"El tamanio del tipo de double ulong es {sizeof(double)}");
+
+////Console.WriteLine($"{0.3 - 0.2}");
+
+
+//int a = 5;
+//Console.WriteLine($"El valor de a es {a}");
+//Console.WriteLine($"El valor de a al aplicar a++ es {a++}");
+//Console.WriteLine($"El valor de a ahora es {a}");
+//Console.WriteLine($"El valor de a al aplicar ++a es {++a}");
+//Console.WriteLine($"El valor de a ahora es {a}");
+//int b = 11;
+//Console.WriteLine(b/a);
+//double aa = 7;
+//double bb = 11;
+//Console.WriteLine(bb / aa);
+//a = b;
+//b = 32;
+//Console.WriteLine($"a = {a}, b = {b}");
+
+//Console.WriteLine("El valor de a es " + a.ToString() + " en este momento");
+//Console.WriteLine("El valor de a es {0} en este momento, y el valor de b es {1}", a, b);
+
+//Console.WriteLine($"El tamanio de un caracter (char) es de {sizeof(char)}");
+
+//char x = 'A';
+//char emoji = '\u263A';
+//char japaneseKanji = '日';
+//Console.WriteLine(emoji);
+//Console.WriteLine(japaneseKanji);
+
+//string cadena = "Hola Mundo";
+//cadena = "Adios Mundo cruel";
+
+//Console.WriteLine($"El tamanio de un booleano (bool) es de {sizeof(bool)}");
+
+
+//// Condicionales
+//short edadUsuario = 70;
+//short edadMayorDeEdad = 18;
+//short edadJubilatoria = 65;
+
+//bool esMayorDeEdad = edadUsuario >= edadMayorDeEdad;
+//bool estaJubilado = edadUsuario >= edadJubilatoria;
+
+//if(esMayorDeEdad)
+//{
+//    if(estaJubilado)
+//    {
+//        Console.WriteLine("Si queres, podes dejar de trabajar");
+//    }
+//    Console.WriteLine("Felicidades, podes tener un trabajo en blanco");
+//} else
+//{
+//    Console.WriteLine("Todavia no trabajes, termina el colegio");
+//}
+
+//Ejemplo en vivo
+
+//int cantidadProductosVendidos;
+//int cantidadDeProductosEnDeposito = 50;
+//int stock;
+
+//Console.WriteLine("Ingrese la cantidad de productos vendidos");
+//cantidadProductosVendidos = Convert.ToInt32(Console.ReadLine());
+//if(cantidadProductosVendidos > cantidadDeProductosEnDeposito)
+//{
+//    Console.WriteLine("No se pueden vender tantos productos");
+//} 
+//else
+//{
+//    stock = cantidadDeProductosEnDeposito - cantidadProductosVendidos;
+
+//    if (stock == 0)
+//    {
+//        Console.WriteLine("Reponer stock de productos");
+//        cantidadDeProductosEnDeposito = cantidadDeProductosEnDeposito + 10;
+//        Console.WriteLine("Productos cargados al deposito");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Informar al área de ventas que quedan {stock} productos sin vender");
+//    }
+//}
+
+//Console.WriteLine("Defina su categoria: ");
+//char categoria = Console.ReadKey().KeyChar;
+//Console.WriteLine();
+//switch (categoria)
+//{
+//    case 'A':
+//        Console.WriteLine("Sos un administrativo");
+//        break;
+//    case 'B':
+//        Console.WriteLine("Sos un empleado profesional");
+//        break;
+//    case 'C':
+//    case 'D':
+//        Console.WriteLine("Empleado de maestranza");
+//        break;
+//    default:
+//        Console.WriteLine("Sin categoria");
+//        break;
+//}
+
+//Ejemplo en vivo
+Console.WriteLine("BIENVENIDOS A LA UNIVERSIDAD CODERHOUSE!!");
+Console.WriteLine("EL MEJOR LUGAR PARA ESTUDIAR DEL MUNDO!");
+
+Console.Write("Ingrese la comision (A, B, C): ");
+char comision = Console.ReadKey().KeyChar;
+Console.WriteLine();
+switch (comision)
+{
+    case 'A':
+    case 'a':
+        Console.WriteLine("Turno mañana");
+        break;
+    case 'B':
+    case 'b':
+        Console.WriteLine("Turno tarde");
+        break;
+    case 'C':
+    case 'c':
+        Console.WriteLine("Turno noche");
+        break;
+    default:
+        Console.WriteLine("Sin turno fijo");
+        break;
+}
+
+Console.WriteLine("Ingrese la nota del alumno: ");
+int nota = Convert.ToInt32(Console.ReadKey().KeyChar.ToString());
+Console.WriteLine();
+if(nota > 7)
+{
+    Console.WriteLine("Promociona la materia");
+}
+else
+{
+    if (nota > 4)
+    {
+        Console.WriteLine("Regular");
+    }
+    else
+    {
+        Console.WriteLine("Desaprobó");
+    }
+}
---
+++ Clase3/Program.cs
@@ -0,0 +1,246 @@
+﻿//string cadena1 = "Hola";
+//string cadena2 = "Mundo";
+
+//string concatenacion = cadena1 + " " + cadena2;
+
+//string cadenaVacia = string.Empty; // ""
+
+
+//string nombrecompleto = string.Empty;
+//Console.WriteLine("Cual es su primer nombre?");
+//string primerNombre = Console.ReadLine();
+//nombrecompleto = nombrecompleto + primerNombre;
+//Console.WriteLine("Tiene segundo nombre?");
+//string tieneSegundoNombre = Console.ReadLine();
+//if(tieneSegundoNombre == "S")
+//{
+//    Console.WriteLine("Cual es su segundo nombre?");
+//    string segundoNombre = Console.ReadLine();
+//    nombrecompleto = nombrecompleto + " " + segundoNombre;
+//}
+
+//Console.WriteLine("Cual es su apellido?");
+//string apellido = Console.ReadLine();
+//nombrecompleto = nombrecompleto + " " + apellido;
+
+//Console.WriteLine("Su nombre completo es " + nombrecompleto);
+
+
+//Console.WriteLine(cadenaVacia);
+
+//string unaCadenaNula = null; //<- NO tiene valor todavia, es null
+
+//string unaCadenaVacia = string.Empty;
+
+//if(unaCadenaNula == unaCadenaVacia)
+//{
+//    Console.WriteLine("me da lo mismo");
+//}
+//else
+//{
+//    Console.WriteLine("Hay que tener cuidado");
+//}
+
+//unaCadenaNula = unaCadenaNula + "otro valor";
+//Console.WriteLine(unaCadenaNula);
+
+//Length
+
+//string cadena = "Hola Mundo";
+
+//Console.WriteLine(cadena.Length);
+//string unaCadenaVaciaConStringEmpty = string.Empty;
+//string unaCadenaVacia = "";
+//string unaCadenaNula = null;
+//Console.WriteLine(unaCadenaNula.Length);
+
+
+//Métodos
+//string miNombre = "Fabrizio";
+//string miNombrePeroGritado = miNombre.ToUpper();
+//Console.WriteLine(miNombrePeroGritado);
+//string miNombrePeroSusurrado = miNombre.ToLower();
+//Console.WriteLine(miNombrePeroSusurrado);
+//Console.WriteLine(miNombre);
+//if(miNombrePeroGritado == miNombrePeroSusurrado)
+//{
+//    Console.WriteLine("Son lo mismo");
+//}
+//else
+//{
+//    Console.WriteLine("No es lo mismo gritar que susurrar");
+//}
+
+
+//to and from string
+
+//int unNumero = Convert.ToInt32("25");
+
+//Console.WriteLine(unNumero);
+
+//unNumero = int.Parse("98");
+
+//Console.WriteLine(unNumero);
+
+//short edad = 32;
+
+//bool esVacaciones = false;
+//Console.WriteLine("Tengo " + edad.ToString() + esVacaciones.ToString() + " años, ya son vacaciones?" );
+
+
+//Contains e Equals
+
+//string cadena1 = "Hola Mundo";
+//string cadena2 = "Mundo";
+//bool estaContenido = cadena1.Contains(cadena2);
+//if(estaContenido)
+//{
+//    Console.WriteLine("Cadena 2 está contenida en cadena 1");
+//}
+
+//string miNombre = "Fabrizio";
+//string miNombrePeroGritado = miNombre.ToUpper();
+//Console.WriteLine(miNombrePeroGritado);
+//string miNombrePeroSusurrado = miNombre.ToLower();
+//Console.WriteLine(miNombrePeroSusurrado);
+//Console.WriteLine(miNombre);
+//bool gritarYSusurrarEsLoMismo = miNombrePeroGritado.Equals(miNombrePeroSusurrado, StringComparison.InvariantCultureIgnoreCase);
+//if (gritarYSusurrarEsLoMismo)
+//{
+//    Console.WriteLine("Son lo mismo");
+//}
+//else
+//{
+//    Console.WriteLine("No es lo mismo gritar que susurrar");
+//}
+
+//if (miNombrePeroGritado.ToUpper() == miNombrePeroSusurrado.ToUpper())
+//{
+//    Console.WriteLine("Son lo mismo");
+//}
+//else
+//{
+//    Console.WriteLine("No es lo mismo gritar que susurrar");
+//}
+
+
+//int unEntero = int.Parse("32");
+//double unDoble = double.Parse("32.5");
+//Console.WriteLine(unDoble);
+//Console.WriteLine(unEntero);
+
+//bool esEntero = int.TryParse("hola", out int valor);
+
+//if(esEntero)
+//{
+//    Console.WriteLine(valor);
+//}
+//else
+//{
+//    Console.WriteLine("No se pudo parsear");
+//}
+
+//string cadenaVacia = string.Empty;
+//string cadenaNula = null;
+//string cadenaConValor = "Hola mundo";
+
+//bool tieneValor = string.IsNullOrEmpty(cadenaConValor);
+//Console.WriteLine(tieneValor);
+
+//int x = 0;
+//while(true)
+//{
+//    Console.WriteLine( x );
+//    //x++;
+//}
+
+// Ejemplo while
+
+//Console.WriteLine("Ingrese contraseña: ");
+//string contraseña = Console.ReadLine();
+//int posibilidades = 3;
+//while(!contraseña.Equals("SuperSecreto") && posibilidades > 0)
+//{
+//    Console.WriteLine("Su contraseña es incorrecta, ingresela nuevamente.");
+//    Console.WriteLine("Ingrese contraseña: ");
+//    contraseña = Console.ReadLine();
+//    posibilidades--;
+//}
+//if(posibilidades == 0)
+//{
+//    Console.WriteLine("Cuenta bloqueada");
+//}
+//else
+//{
+//    Console.WriteLine("Bienvenido!");
+//}
+
+
+//for (int i = 0; i < 10; i++ )
+//{
+//    Console.WriteLine( i );
+//}
+
+//int j = 0;
+//while( j < 10 )
+//{
+//    Console.WriteLine( j );
+//    j++;
+//}
+
+//Ejercicio promedios
+
+//int i;
+//int cantidadDeNotasACargar = 0;
+//double promedioNotas =  0;
+//double nota;
+//double sumaNotas = 0;
+//// promedio = sumaNotas / cantidadDeNotas
+
+//Console.WriteLine("Ingrese la cantidad de notas a cargar");
+//cantidadDeNotasACargar = int.Parse(Console.ReadLine());
+
+//for(i = 1; i <= cantidadDeNotasACargar; i++)
+//{
+//    Console.WriteLine("Ingrese una nota:");
+//    nota = double.Parse(Console.ReadLine());
+//    sumaNotas = sumaNotas + nota;
+//}
+//promedioNotas = sumaNotas / cantidadDeNotasACargar;
+//Console.WriteLine("El promedio de notas cargadas es " + promedioNotas);
+
+//int i = 0;
+//while (true)
+//{
+//    i++;
+//    if(i == 489)
+//    {
+//        continue;
+//    }
+//    Console.WriteLine(i);
+//    if(i == 500)
+//    {
+//        break;
+//    }
+//}
+
+//for(int i = 0; i <= 500; i++)
+//{
+//    if(i != 489)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+int i = 501;
+do
+{
+    i++;
+    Console.WriteLine(i);
+} while (i < 500);
+
+//int i = 501;
+//while (i < 500)
+//{
+//    i++;
+//    Console.WriteLine(i);
+//}
