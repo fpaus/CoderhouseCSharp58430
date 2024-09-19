9091234a957272bf688841b38c6753e535f19878
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2024-09-18 22:34:49 -0300

    Add migrations

--- Clase11/Contexts/CoderhouseDbContext.cs
+++ Clase11/Contexts/CoderhouseDbContext.cs
@@ -9,16 +9,16 @@ public class CoderhouseDbContext : DbContext
     public DbSet<Producto> Productos { get; set; }
     public DbSet<Usuario> Usuarios { get; set; }
 
-    public CoderhouseDbContext()
-        : base() { }
+    //public CoderhouseDbContext()
+    //    : base() { }
 
-    public CoderhouseDbContext(DbContextOptions<CoderhouseDbContext> options)
-        : base(options) { }
+    //public CoderhouseDbContext(DbContextOptions<CoderhouseDbContext> options)
+    //    : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
-            "Data Source=10.0.2.2;Initial Catalog=Preparacion;User ID=sa;Password=Str0ngPassword;TrustServerCertificate=True"
+            "Data Source=10.0.2.2;Initial Catalog=Coderhouse;User ID=sa;Password=Str0ngPassword;TrustServerCertificate=True"
         );
     }
 }
