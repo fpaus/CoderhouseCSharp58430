e84e246841e09e124111d61d1616c6499793002f
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2024-09-30 22:29:12 -0300

    Clase 14

---
+++ Capas/WebApi/Controllers/ProductosController.cs
@@ -0,0 +1,39 @@
+﻿using Bussiness.Services;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ILogger<ProductosController> _logger;
+        private readonly ProductosService _productosService;
+
+        public ProductosController( ILogger<ProductosController> logger, ProductosService productosService) {
+            _logger = logger;
+            _productosService = productosService;
+        }
+
+        [HttpGet(Name = "Get Productos")]
+        public ActionResult<List<Producto>> GetProductos()
+        {
+
+            return _productosService.GetProductos();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Producto> GetProducto(int id)
+        {
+            _logger.LogInformation("Consultando por el producto con id {id}", id);
+            var producto = _productosService.GetOneProducto(id);
+            if(producto is null)
+            {
+                return NotFound();
+            }
+            return producto;
+        }
+    }
+}
---
+++ Capas/WebApi/Controllers/UsuariosController.cs
@@ -0,0 +1,40 @@
+﻿using Bussiness.Services;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly ILogger<UsuariosController> _logger;
+        private readonly UsuariosService _usuariosService;
+
+        public UsuariosController(ILogger<UsuariosController> logger, UsuariosService usuariosService)
+        {
+            _logger = logger;
+            _usuariosService = usuariosService;
+        }
+
+        [HttpGet(Name = "Get Usuarios")]
+        public ActionResult<List<Usuario>> GetUsuarios()
+        {
+
+            return _usuariosService.GetUsuarios();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Usuario> GetUsuario(int id)
+        {
+            _logger.LogInformation("Consultando por el producto con id {id}", id);
+            var usuario = _usuariosService.GetOneUsuario(id);
+            if (usuario is null)
+            {
+                return NotFound();
+            }
+            return usuario;
+        }
+    }
+}
